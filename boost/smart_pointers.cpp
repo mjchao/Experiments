76e6a86d1a10742dfec607eeca8ca99342799fcf
Mickey Chao <mjchao2@gmail.com>
2018-12-24 17:29:36 +0000

    Add tests with scoped pointer.

--- boost/smart_pointers.cpp
+++ boost/smart_pointers.cpp
@@ -1,6 +1,30 @@
+#include <assert.h>
+#include <boost/scoped_array.hpp>
 #include <boost/scoped_ptr.hpp>
+#include <iostream>
+
+
+void testShared() {
+
+}
+
+
+void testScoped() {
+  boost::scoped_ptr<int> int_ptr(new int(1));
+  assert(*int_ptr == 1);
+
+  boost::scoped_array<int> array_ptr(new int[10]);
+  for (int i = 0; i < 10; ++i) {
+    array_ptr[i] = i;
+  }
+  for (int i = 0; i < 10; ++i) {
+    assert(array_ptr[i] == i);
+  }
+}
 
 int main() {
+  testScoped();
+  testShared();
   return 0;
 }
 
