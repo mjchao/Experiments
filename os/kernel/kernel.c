c23f682d578a7cc0cade6345386a2d6d59b1c999
Mickey Chao <mjchao2@gmail.com>
2019-01-02 21:34:45 -0600

    Write X at different location on screen.

--- os/kernel/kernel.c
+++ os/kernel/kernel.c
@@ -1,12 +1,20 @@
+#include "drivers/ports.h"
+
 // ???
 void entrypoint() {
 }
 
 void main() {
-  // video memory is at 0xb8000 and points to top-left of screen
-  char* video_memory = (char*) 0xb8000;
-  
-  // write 'X' to top-left of screen
-  *video_memory = 'X';
+  port_byte_out(0x3d4, 14);
+  int position = port_byte_in(0x3d5);
+  position = position << 8;
+
+  port_byte_out(0x3d4, 15);
+  position += port_byte_in(0x3d5);
+
+  int offset_from_vga = position * 2;
+  char *vga = 0xb8000;
+  vga[offset_from_vga] = 'X';
+  vga[offset_from_vga + 1] = 0x0f;
 }
 
